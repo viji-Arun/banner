51cf121c3f3ee204a3d4db16842f95f430917e7f
viji-Arun <vijayasreearu22@gmail.com>
2024-03-14 06:58:36 -0700

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import BannerCardItem from './components/BannerCardItem.js'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const bannerCardsList = [
+  {
+    id: 1,
+    headerText: 'The Seasons Latest',
+    description: 'Get the seasons all latest designs in a flick of your hand',
+    className: 'card-1',
+  },
+  {
+    id: 2,
+    headerText: 'Our New Designs',
+    description:
+      'Get the designs developed by our in-house team all for yourself',
+    className: 'card-2',
+  },
+  {
+    id: 3,
+    headerText: 'Insiders',
+    description: 'Get the top class products for yourself with an extra off',
+    className: 'card-3',
+  },
+]
 
-export default App;
+const App = () => (
+  <div className="bg-container">
+    <ul className="banner-card-list">
+      {bannerCardsList.map(eachcard => (
+        <BannerCardItem bannerCard={eachcard} key={eachcard.id} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
---
+++ src/components/BannerCardItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+const BannerCardItem = props => {
+  const {bannerCard} = props
+  const {headerText, description, className} = bannerCard
+  return (
+    <li className={`card ${className}`}>
+      <div>
+        <h1 className="heading">{headerText}</h1>
+        <p className="description">{description}</p>
+        <button className="button" type="button">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
